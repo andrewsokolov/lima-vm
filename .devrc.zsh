f69080242a5f4bfce33d6e6968adeb6049b74b2c
Andrew Sokolov <asokolov@atlassian.com>
2024-09-21 13:43:43 +0200

    add ssh generator

--- .devrc.zsh
+++ .devrc.zsh
@@ -1,5 +1,6 @@
 alias bat='batcat'
 alias docker='nerdctl'
 alias python='python3'
+alias myssh='cat ~/.ssh/id_rsa.pub'
 alias docker-compose='nerdctl compose'
 export PATH="/home/$USER.linux/.local/bin:$PATH"
--- scripts/init.sh
+++ scripts/init.sh
@@ -9,19 +9,7 @@ lima zsh -c 'echo "install poetry"'
 lima zsh -c 'curl -sSL https://install.python-poetry.org | python3 -'
 lima zsh -c "mkdir -p ~/.ssh"
 
-# lima sh -c 'if [ -f "$(HOME_DIR)/$USER/.ssh/id_rsa" ]; then \
-#     cp $(HOME_DIR)/$USER/.ssh/id_rsa ~/.ssh/id_rsa; \
-#     cp $(HOME_DIR)/$USER/.ssh/id_rsa.pub ~/.ssh/id_rsa.pub; \
-# else  \
-#     echo "$(HOME_DIR)/$USER/.ssh/id_rsa not found"; \
-# fi'
-
-# lima sh -c 'if [ -f "$HOME_DIR/$USER/.ssh/id_ed25519" ]; then \
-#     cp $HOME_DIR/$USER/.ssh/id_ed25519 ~/.ssh/id_ed25519; \
-#     cp $HOME_DIR/$USER/.ssh/id_ed25519.pub ~/.ssh/id_ed25519.pub; \
-# else  \
-#     echo "$HOME_DIR/$USER/.ssh/id_ed25519 not found"; \
-# fi'
+lima sh -c 'ssh-keygen -t rsa -b 4096 -C "mr.andrewsokolov@gmail.com" -f ~/.ssh/id_rsa -N ""'
 
 lima sh -c 'if ! grep -q "source $(pwd)/.devrc.zsh" ~/.zshrc; then \
     echo "source $(pwd)/.devrc.zsh" >> ~/.zshrc; \
